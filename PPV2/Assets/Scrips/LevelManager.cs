7f3a93e9465d152114c29199998f70abf27e45cc
BrandonGonzal <159967647+BrandonGonzal@users.noreply.github.com>
2024-03-09 18:46:07 -0600

    main-Options

--- PPV2/Assets/Scrips/LevelManager.cs
+++ PPV2/Assets/Scrips/LevelManager.cs
@@ -1,19 +1,75 @@
  using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
 
 public class LevelManager : MonoBehaviour
 {
-    public ScriptableObject Lesson;
+    [Header("Level Data")]
+    public Subject Lesson;
+
+    [Header("User Interface")]
+    public TMP_Text QuestionTxt;
+    public List<Option> Options;
+
+    [Header("Game Configuration")]
+    public int questionAmount = 0;
+    public int currentQuestion = 0;
+    public string question;
+    public string correctAnswer;
+
+    [Header("Current Leccion")]
+    public Leccion currentLesson;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Establecemos la cantidad de preguntas en la leccion
+        questionAmount = Lesson.leccionList.Count;
+
+
+        //Cargar la primera pregunta
+        LoadQuestion();
+
     }
 
-    // Update is called once per frame
-    void Update()
+
+    private void LoadQuestion() 
     {
-        
+        //Aseguramos que la pregunta actual este dentro de los limites
+        if (currentQuestion < questionAmount)
+        {
+            //Establecemos la leccion actual
+            currentLesson = Lesson.leccionList[currentQuestion];
+            //Establecemos la pregunta
+            question = currentLesson.lessons;
+            //Establecemos la respuesta correcta
+            correctAnswer = currentLesson.options[currentLesson.correctAnswer];
+            //Establecemos la pregunta en la UI
+            QuestionTxt.text = question;
+        }
+        else 
+        {
+            //Si llegamos al fin de las preguntas
+            Debug.Log("Fin de las preguntas");
+       }
+    }
+
+    public void NextQuestion() 
+    {
+        if (currentQuestion < questionAmount)
+        {
+            //Incrementamos el indice de la pregunta actual
+            currentQuestion++;
+           //Cargamos la nueva pregunta
+         LoadQuestion();
+
+        }
+        else
+        {
+            //cambio de escena
+        }
     }
 }
